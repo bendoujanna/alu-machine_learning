462623e65873d46dcdfb85547c016b5d77c9b760
bendoujanna <b.vitalina@alustudent.com>
2026-09-13 00:06:32 +0200

    minor changes

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -3,7 +3,8 @@
 
 
 def add_arrays(arr1, arr2):
-    """Add two arrays element-wise and return a new list, or None if shapes differ."""
+    """Add two arrays element-wise and return a new list,
+    or None if shapes differ."""
     if len(arr1) != len(arr2):
         return None
     return [arr1[i] + arr2[i] for i in range(len(arr1))]
